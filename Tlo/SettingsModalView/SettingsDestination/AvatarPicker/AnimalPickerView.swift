11b14c2239eebe5b7648359959c214cec2784f0e
sye7qjm3ac <sye7qjm3ac@icloud.com>
2026-01-27 00:16:32 +0700

    feat: avatar picker scroll
    must be modified

---
+++ Tlo/SettingsModalView/SettingsDestination/AvatarPicker/AnimalPickerView.swift
@@ -0,0 +1,34 @@
+//
+//  AnimalPickerView.swift
+//  Tlo
+//
+//  Created by Eyhciurmrn Zmpodackrl on 27.01.2026.
+//
+
+import SwiftUI
+
+struct AnimalPickerView: View {
+  @Binding var animal: AnimalType
+  private let avatarSize: CGFloat = 80
+  
+  var body: some View {
+    HStack(spacing: 5) {
+      ForEach(AnimalType.allCases, id:\.self) { animal in
+        AnimalAvatarView(animal: animal, circleColor: .mainNopoopBackground.mix(with: .gray, by: 0.2), withBacking: .white)
+          .addStroke(lineWidth: animal == self.animal ? 2 : 0)
+          .frame(width: avatarSize, height: avatarSize)
+          .onTapGesture {
+            withAnimation(.easeInOut(duration: 0.3)) {
+              self.animal = animal
+            }
+          }
+      }
+    }
+    .padding(15)
+  }
+}
+
+#Preview {
+  @Previewable @State var userSession = UserSession()
+  AnimalPickerView(animal: $userSession.selectedAvatar.animal)
+}
